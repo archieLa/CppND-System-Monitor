b076eecfbce98da1afbb422862370e6650d4bb00
archieLa <arturm.lach@gmail.com>
2019-07-27 21:08:04 +0000

    Started working on Process.h

--- Process.h
+++ Process.h
@@ -5,43 +5,56 @@ using namespace std;
 Basic class for Process representation
 It contains relevant attributes as shown below
 */
-class Process {
-private:
-    string pid;
-    string user;
-    string cmd;
-    string cpu;
-    string mem;
-    string upTime;
+class Process 
+{   
+    private:
+        string pid_;
+        string user_;
+        string cmd_;
+        string cpu_;
+        string mem_;
+        string upTime_;
 
-public:
-    Process(string pid){
-        this->pid = pid;
-        this->user = ProcessParser::getProcUser(pid);
-        //TODOs:
-        //complete for mem
-        //complete for cmd
-        //complete for upTime
-        //complete for cpu
-    }
-    void setPid(int pid);
-    string getPid()const;
-    string getUser()const;
-    string getCmd()const;
-    int getCpu()const;
-    int getMem()const;
-    string getUpTime()const;
-    string getProcess();
+    public:
+        Process(const string& pid)
+        {
+            pid_ = pid;
+            user_ = ProcessParser::getProcUser(pid);
+            cmd_ = ProcessParser::getCmd(pid);
+            cpu_ = ProcessParser::getCpuPercent(pid);
+            mem_ = ProcessParser::getVmSize(pid);
+            upTime_ = ProcessParser::getProcUpTime(pid);
+        }
+    
+
+    string getPid() const;
+    string getUser() const;
+    string getCmd() const;
+    
+    int getCpu() const;
+    int getMem() const;
+    string getUpTime() const;
+    string getProcess() ;
 };
-void Process::setPid(int pid){
-    this->pid = pid;
-}
-string Process::getPid()const {
+
+
+string Process::getPid() const 
+{
     return this->pid;
 }
+
+string Process::getUser() const
+{
+    return user_;
+}
+
+
 string Process::getProcess(){
     if(!ProcessParser::isPidExisting(this->pid))
+    {
         return "";
+    }
+
     this->mem = ProcessParser::getVmSize(this->pid);
     this->upTime = ProcessParser::getProcUpTime(this->pid);
     this->cpu = ProcessParser::getCpuPercent(this->pid);
